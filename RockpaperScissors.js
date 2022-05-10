dc8dc0aa1d36ba6738fb5e15b1a5bde55b8e3ac4
Raven <user@users-MacBook-Air.local>
2022-05-10 06:10:55 -0400

    second required update

---
+++ RockpaperScissors.js
@@ -0,0 +1,11 @@
+const playerChoice = document.createElement('h1')
+const cpuChoice = document.createElement('h1')
+const resultPlayer = document.createElement('h1')
+const gameGrid = document.getElementById('game')
+gameGrid.append(playerChoice, cpuChoice, resultPlayer)
+//Checked via Live-server successful
+
+const choices = ['paper', 'scissors', 'rock']
+// declaring choices
+
+
